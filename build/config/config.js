1f1e9fa587935ac93ce63b8264df9f3a09c4e1eb
Mohit Durve <mohitmilind.durve@allscripts.com>
2025-05-11 14:31:27 -0700

    build-changes

--- build/config/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    practiceFusion: {
-        baseUrl: process.env.PF_API_URL || 'https://api.practicefusion.com',
-        clientId: process.env.PF_CLIENT_ID || '',
-        clientSecret: process.env.PF_CLIENT_SECRET || '',
-        redirectUri: process.env.PF_REDIRECT_URI || ''
-    }
-};
